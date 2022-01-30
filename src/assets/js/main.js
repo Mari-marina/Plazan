eb5027114a95faaf933a76d9271b7ddd8df380d9
Mari-marina <shunkina9292@mail.ru>
2022-01-31 01:02:31 +0300

    reviews

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -14,23 +14,24 @@ function burgerMenu() {
 }
 burgerMenu();
 
-let anchors = document.querySelectorAll("a");
-for (let anchor of anchor) {
-  anchor.addEventListener("click", function (event) {
-    event.preventDefault();
-    let blockID = anchor.getAttribute("href");
-    document.querySelector(blockID).scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  });
-}
+// // let anchors = document.querySelectorAll('a[href*="#"]');
+// let anchors = document.querySelectorAll(".go_top");
+// for (let anchor of anchor) {
+//   anchor.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     let blockID = anchor.getAttribute("href");
+//     document.querySelector(blockID).scrollIntoView({
+//       behavior: "smooth",
+//       block: "start",
+//     });
+//   });
+// }
 
-const goTop = document.querySelector(".go_top");
-window.addEventListener("scroll", function () {
-  if (window.scrollY > 0) {
-    goTop.style.display = "block";
-  } else {
-    goTop.style.display = "none";
-  }
-});
+// const goTop = document.querySelector(".go_top");
+// window.addEventListener("scroll", function () {
+//   if (window.scrollY > 0) {
+//     goTop.style.display = "block";
+//   } else {
+//     goTop.style.display = "none";
+//   }
+// });
