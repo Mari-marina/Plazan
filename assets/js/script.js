780344a0df68f20ebf8949805150ad12cb73b291
Mari-marina <shunkina9292@mail.ru>
2022-02-07 20:05:03 +0300

    Update 2022-02-07T17:04:59.947Z

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ function test(a, b) {
 //навигация, меню
 const button = document.querySelector(".burger");
 let menu = document.querySelector(".nav");
+let burgerButton = document.querySelector(".burger");
 
 button.addEventListener("click", function () {
   menu.classList.toggle("active");
@@ -19,7 +20,6 @@ overlay.addEventListener("click", function () {
 });
 
 function burgerMenu() {
-  let burgerButton = document.querySelector(".burger");
   burgerButton.addEventListener("click", function () {
     burgerButton.classList.toggle("burger_active");
   });
